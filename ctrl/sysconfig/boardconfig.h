9a0695c397541c76335f60306d9eee7a7c6db49a
kyChu <hello.kychu@gmail.com>
2020-02-17 16:30:03 +0800

    update memory info

--- ctrl/sysconfig/boardconfig.h
+++ ctrl/sysconfig/boardconfig.h
@@ -8,17 +8,17 @@
 #ifndef BOARDCONFIG_H_
 #define BOARDCONFIG_H_
 
-#define SYS_ARCH_STR                        "ATT EST M0"
+#define SYS_ARCH_STR                        "ATT CTL M0"
 
 #define SYS_XCLK_FREQUENCY                  (8000000)
 
-#define SYS_HEAP_SIZE                       256
+#define SYS_HEAP_SIZE                       0
 #define SYS_STACK_SIZE                      256
 
 #define SYS_BOOT_ORIGIN                     0x08000000
-#define SYS_BOOT_LENGTH                     6K
-#define SYS_TEXT_ORIGIN                     0x08001800
-#define SYS_TEXT_LENGTH                     26K
+#define SYS_BOOT_LENGTH                     8K
+#define SYS_TEXT_ORIGIN                     0x08002000
+#define SYS_TEXT_LENGTH                     120K
 #define SYS_SRAM_ORIGIN                     0x20000000
 #define SYS_SRAM_LENGTH                     6K
 
