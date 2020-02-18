fc54776144fb3904537cf6be15aea744009342fb
kyChu <hello.kychu@gmail.com>
2020-02-19 02:20:07 +0800

    update pwm driver

--- ctrl/driver/periph/pwm.c
+++ ctrl/driver/periph/pwm.c
@@ -8,44 +8,44 @@
 #include "pwm.h"
 
 /**
-  * @brief  Configure the TIM2 PWM Pin.
+  * @brief  Configure the PWM Pin.
   * @param  None
   * @retval None
   */
-void pwm_init(uint8_t d)
+void pwm_init(void)
 {
-  TIM_TimeBaseInitTypeDef  TIM_TimeBaseStructure;
-  TIM_OCInitTypeDef  TIM_OCInitStructure;
+  TIM_TimeBaseInitTypeDef TIM_TimeBaseStructure;
+  TIM_OCInitTypeDef TIM_OCInitStructure;
   GPIO_InitTypeDef GPIO_InitStructure;
 
-  /* GPIOA Clocks enable */
-  RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOA, ENABLE);
+  /* PWM_GPIO_PORT Clocks enable */
+  RCC_AHBPeriphClockCmd(PWM_GPIO_CLK, ENABLE);
 
-  /* GPIOA Configuration: GPIO_PIN_1 as alternate function push-pull */
-  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_1;
+  /* PWM_GPIO_PORT Configuration: PWM_GPIO_PIN as alternate function push-pull */
+  GPIO_InitStructure.GPIO_Pin = PWM_GPIO_PIN;
   GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
-  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_Level_3;
   GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
   GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP ;
-  GPIO_Init(GPIOA, &GPIO_InitStructure);
+  GPIO_Init(PWM_GPIO_PORT, &GPIO_InitStructure);
 
-  GPIO_PinAFConfig(GPIOA, GPIO_PinSource1, GPIO_AF_2);
+  GPIO_PinAFConfig(PWM_GPIO_PORT, PWM_GPIO_PinSource, PWM_GPIO_PIN_AF);
 
-  /* TIM2 Configuration ---------------------------------------------------
+  /* Timer Configuration ---------------------------------------------------
    Generate 1 PWM signal with 0% duty cycles:
-   TIM2 input clock (TIM2CLK) is set to APB1 clock (PCLK1)
-    => TIM2CLK = PCLK1 = SystemCoreClock
-   TIM2CLK = SystemCoreClock, Prescaler = 47, TIM2 counter clock = 1MHz
+   Timer input clock (TIMxCLK) is set to APB1 clock (PCLK1)
+    => TIMxCLK = PCLK1 = SystemCoreClock
+   TIMxCLK = SystemCoreClock, Prescaler = 47, TIMx counter clock = 1MHz
    SystemCoreClock is set to 48 MHz for STM32F0xx devices
 
-   TIM2_Period = (1MHz / 10KHz) - 1 = 99
-   The channel 2 duty cycle is set to x%
+   TIMx_Period = (1MHz / 10KHz) - 1 = 99
+   The duty cycle is set to 0% as default.
    The Timer pulse is calculated as follows:
-     - Channel2Pulse = DutyCycle * (TIM2_Period - 1) / 100
+     - ChannelyPulse = DutyCycle * (TIMx_Period - 1) / 100
   ----------------------------------------------------------------------- */
 
-  /* TIM2 clock enable */
-  RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2 , ENABLE);
+  /* timer clock enable */
+  PWM_TIM_CLK_EN();
 
   /* Time Base configuration */
   TIM_TimeBaseStructure.TIM_Prescaler = 47;
@@ -54,10 +54,10 @@ void pwm_init(uint8_t d)
   TIM_TimeBaseStructure.TIM_ClockDivision = 0;
   TIM_TimeBaseStructure.TIM_RepetitionCounter = 0;
 
-  TIM_TimeBaseInit(TIM2, &TIM_TimeBaseStructure);
+  TIM_TimeBaseInit(PWM_TIM, &TIM_TimeBaseStructure);
 
-  /* Channel 2 Configuration in PWM mode */
-  TIM_OCInitStructure.TIM_Pulse = d;
+  /* PWM_TIM Channel Configuration in PWM mode */
+  TIM_OCInitStructure.TIM_Pulse = 0;
   TIM_OCInitStructure.TIM_OCMode = TIM_OCMode_PWM2;
   TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable;
   TIM_OCInitStructure.TIM_OutputNState = TIM_OutputNState_Enable;
@@ -66,14 +66,14 @@ void pwm_init(uint8_t d)
   TIM_OCInitStructure.TIM_OCIdleState = TIM_OCIdleState_Set;
   TIM_OCInitStructure.TIM_OCNIdleState = TIM_OCIdleState_Reset;
 
-  TIM_OC2Init(TIM2, &TIM_OCInitStructure);
+  PWM_TIM_OCxInit(PWM_TIM, &TIM_OCInitStructure);
 
-  /* TIM2 counter enable */
-  TIM_Cmd(TIM2, ENABLE);
+  /* PWM_TIM counter enable */
+  TIM_Cmd(PWM_TIM, ENABLE);
 }
 
-void pwm_set_dutycycle(uint8_t d)
+void pwm_dutycycle(uint8_t d)
 {
 	if(d > 99) d = 99;
-	TIM2->CCR2 = d;
+	PWM_TIM_SET_CCR(d);
 }
--- ctrl/driver/periph/pwm.h
+++ ctrl/driver/periph/pwm.h
@@ -10,7 +10,19 @@
 
 #include "SysConfig.h"
 
-void pwm_init(uint8_t d);
-void pwm_set_dutycycle(uint8_t d);
+#define PWM_TIM                TIM3
+#define PWM_TIM_CLK_EN()       RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM3, ENABLE)
+
+#define PWM_GPIO_PORT          GPIOB
+#define PWM_GPIO_CLK           RCC_AHBPeriph_GPIOB
+#define PWM_GPIO_PIN           GPIO_Pin_0
+#define PWM_GPIO_PinSource     GPIO_PinSource0
+#define PWM_GPIO_PIN_AF        GPIO_AF_1
+
+#define PWM_TIM_OCxInit        TIM_OC3Init
+#define PWM_TIM_SET_CCR(x)     do { PWM_TIM->CCR3= x; } while(0);
+
+void pwm_init(void);
+void pwm_dutycycle(uint8_t d);
 
 #endif /* PERIPH_PWM_H_ */
