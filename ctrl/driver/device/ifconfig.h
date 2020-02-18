77d7dcaf8be33caa83f37fb2d15f66df22a662e0
kyChu <hello.kychu@gmail.com>
2020-02-19 02:43:43 +0800

    correct api

--- ctrl/driver/device/ifconfig.h
+++ ctrl/driver/device/ifconfig.h
@@ -8,7 +8,6 @@
 #ifndef DRIVER_DEVICE_IFCONFIG_H_
 #define DRIVER_DEVICE_IFCONFIG_H_
 
-#include "i2c1.h"
 #include "spi1.h"
 #include "intio.h"
 #include "uart2.h"
@@ -18,7 +17,7 @@
 #define USER_UART_TX_BYTE                   uart2_TxByte
 #define USER_UART_TX_BYTES                  uart2_TxBytesDMA
 
-#define USER_PWM_SET_DUTYCYCLE              pwm_set_dutycycle
+#define USER_PWM_SET_DUTYCYCLE              pwm_dutycycle
 
 #define USER_LED_ON                         USER_IO_LOW
 #define USER_LED_OFF                        USER_IO_HIGH
