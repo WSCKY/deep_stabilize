eacb0c9afb5657242373997e25d6215bbf548462
kyChu <hello.kychu@gmail.com>
2020-02-19 02:19:38 +0800

    use tim6 for counter

--- ctrl/startup/TimerCounter.c
+++ ctrl/startup/TimerCounter.c
@@ -13,6 +13,11 @@
 
 /* Private typedef -----------------------------------------------------------*/
 /* Private define ------------------------------------------------------------*/
+#define TC_TIMER               TIM6
+#define TC_TIMER_CLK_EN()      RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM6, ENABLE)
+#define TC_TIMER_IRQCHAN       TIM6_DAC_IRQn
+#define TC_TIMER_IRQHANDLER    TIM6_DAC_IRQHandler
+
 /* Private macro -------------------------------------------------------------*/
 /* Private variables ---------------------------------------------------------*/
 static uint32_t OverFlowCnt = 0;
@@ -29,36 +34,36 @@ void _TimeTicksInit(void)
 {
 	NVIC_InitTypeDef NVIC_InitStructure;
 	TIM_TimeBaseInitTypeDef TIM_TimeBaseStructure;
-	/* TIM3 clock enable */
-	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM3, ENABLE);
+	/* TC_TIMER clock enable */
+	TC_TIMER_CLK_EN();
 
 	/* Time base configuration */
-	TIM_TimeBaseStructure.TIM_Period = 0xFFFF;
-	TIM_TimeBaseStructure.TIM_Prescaler = (uint16_t) ((SystemCoreClock) / 1000000) - 1;
+	TIM_TimeBaseStructure.TIM_Period = 0xFFFF; /* 16-bit timer */
+	TIM_TimeBaseStructure.TIM_Prescaler = (uint16_t) ((SystemCoreClock) / 1000000) - 1; /* 1us */
 	TIM_TimeBaseStructure.TIM_ClockDivision = 0;
 	TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;
-	TIM_TimeBaseInit(TIM3, &TIM_TimeBaseStructure);
+	TIM_TimeBaseInit(TC_TIMER, &TIM_TimeBaseStructure);
 
 	/* NVIC configuration *******************************************************/
-	NVIC_InitStructure.NVIC_IRQChannel = TIM3_IRQn;
+	NVIC_InitStructure.NVIC_IRQChannel = TC_TIMER_IRQCHAN;
 	NVIC_InitStructure.NVIC_IRQChannelPriority = SYSTEM_TIMER_INT_PRIORITY;
 	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
 	NVIC_Init(&NVIC_InitStructure);
 
-	/* Clear TIM3 Counter */
-	TIM_SetCounter(TIM3, 0);
+	/* Clear TC_TIMER Counter */
+	TIM_SetCounter(TC_TIMER, 0);
 
-	/* TIM Interrupts enable */
-	TIM_ITConfig(TIM3, TIM_IT_Update, ENABLE);
+	/* TC_TIMER Interrupts enable */
+	TIM_ITConfig(TC_TIMER, TIM_IT_Update, ENABLE);
 
-	/* TIM3 counter enable */
-	TIM_Cmd(TIM3, ENABLE);
+	/* TC_TIMER counter enable */
+	TIM_Cmd(TC_TIMER, ENABLE);
 }
 
 /*
  * return timer counter value.
  */
-uint16_t _Get_Ticks(void) { return TIM3->CNT; }
+uint16_t _Get_Ticks(void) { return TC_TIMER->CNT; }
 
 /*
  * time in microsecond.
@@ -67,8 +72,8 @@ uint32_t _Get_Micros(void)
 {
 	uint32_t t1, t2;
 	do{
-		t1 = (OverFlowCnt << 16) + (TIM3->CNT & 0xFFFF);
-		t2 = (OverFlowCnt << 16) + (TIM3->CNT & 0xFFFF);
+		t1 = (OverFlowCnt << 16) + (TC_TIMER->CNT & 0xFFFF);
+		t2 = (OverFlowCnt << 16) + (TC_TIMER->CNT & 0xFFFF);
 	} while(t1 != t2);
 	return t1;
 }
@@ -126,14 +131,14 @@ uint32_t _GetTimeMeasured(void)
 }
 
 /**
-  * @brief  This function handles the TIM3 interrupt request.
+  * @brief  This function handles the TC_TIMER interrupt request.
   * @param  None
   * @retval None
   */
-void TIM3_IRQHandler(void)
+void TC_TIMER_IRQHANDLER(void)
 {
-	if (TIM_GetITStatus(TIM3, TIM_IT_Update) != RESET) {
-		TIM_ClearITPendingBit(TIM3, TIM_IT_Update);
+	if (TIM_GetITStatus(TC_TIMER, TIM_IT_Update) != RESET) {
+		TIM_ClearITPendingBit(TC_TIMER, TIM_IT_Update);
 		OverFlowCnt ++;
 	}
 }
