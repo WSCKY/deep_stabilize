dce45cea1c193734b720704b86e3230ec359cb04
kyChu <hello.kychu@gmail.com>
2020-02-20 18:44:29 +0800

    test uart1,2,3,4 tx function

--- ctrl/apps/main_task.c
+++ ctrl/apps/main_task.c
@@ -23,16 +23,21 @@ void StartThread(void const * arg)
   uart2_init();
   uart3_init();
   uart4_init();
-  _delay_ms(50);
+  delay(50);
   /* The Application layer has only to call USBD_Init to
      initialize the USB low level driver, the USB device library, the USB clock,
      pins and interrupt service routine (BSP) to start the Library*/
   USBD_Init(&USB_Device_dev, &USR_desc, &USBD_CDC_cb, &USR_cb);
-  _delay_ms(50);
+  delay(50);
 
 /*  mpu9250_init();
   _delay_ms(10); */
   for(;;) {
+    delay(1000);
+    uart1_TxBytes((uint8_t *)"1234567890\n", 11);
+    uart4_TxBytes((uint8_t *)"1234567890\n", 11);
+    uart2_TxBytesDMA((uint8_t *)"1234567890\n", 11);
+    uart3_TxBytesDMA((uint8_t *)"1234567890\n", 11);
 //    com_task();
 //    uart2_TxBytesDMA((uint8_t *)"kyChu\n", 6);
 //    if(USBD_isEnabled()) {
