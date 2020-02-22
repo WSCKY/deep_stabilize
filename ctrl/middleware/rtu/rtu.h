ebcfef2594d3d7c8bdf13bd1fb494033d00c68f6
kyChu <hello.kychu@gmail.com>
2020-02-22 19:08:05 +0800

    add function prototype

--- ctrl/middleware/rtu/rtu.h
+++ ctrl/middleware/rtu/rtu.h
@@ -10,6 +10,8 @@
 
 #include "SysConfig.h"
 
+#include <stddef.h>
+
 #define RTU_CMD_READ_MULTI     0x03
 #define RTU_CMD_READ_SINGLE    0x04
 #define RTU_CMD_WRITE_MULTI    0x10
@@ -22,5 +24,10 @@ typedef struct {
   uint32_t cache_size;
   rtuTxBytesFunc tx_bytes;
 } rtu_handle_t;
+
+status_t rtu_read_single(rtu_handle_t *hrtu, uint8_t dev_addr, uint16_t reg_addr);
+status_t rtu_read_multi(rtu_handle_t *hrtu, uint8_t dev_addr, uint16_t reg_addr, uint16_t reg_numb);
+status_t rtu_write_single(rtu_handle_t *hrtu, uint8_t dev_addr, uint16_t reg_addr, uint16_t reg_data);
+status_t rtu_write_multi(rtu_handle_t *hrtu, uint8_t dev_addr, uint16_t reg_addr, uint16_t *reg_data, uint16_t size);
 
 #endif /* __RTU_H_ */
