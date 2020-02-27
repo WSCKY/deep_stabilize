f39a0c6b29949a51f6e5e918a5f3ec181280ce28
kyChu <hello.kychu@gmail.com>
2020-02-27 17:19:04 +0800

    add function prototype

--- ctrl/driver/device/servo.h
+++ ctrl/driver/device/servo.h
@@ -17,4 +17,16 @@ typedef struct {
   rtu_handle_t *hrtu;
 } servo_handle_t;
 
+void servo_init(const servo_handle_t *hsrv);
+status_t servo_detect(const servo_handle_t *hsrv);
+status_t servo_get_position(const servo_handle_t *hsrv, uint32_t *val);
+status_t servo_speed_up_time(const servo_handle_t *hsrv, uint16_t time);
+status_t servo_speed_down_time(const servo_handle_t *hsrv, uint16_t time);
+status_t servo_set_speed(const servo_handle_t *hsrv, uint16_t val);
+status_t servo_get_speed(const servo_handle_t *hsrv, uint16_t *val);
+status_t servo_run_time(const servo_handle_t *hsrv, int32_t time);
+status_t servo_run_pulse(const servo_handle_t *hsrv, int32_t pulse);
+status_t servo_run_position(const servo_handle_t *hsrv, int32_t pos);
+status_t servo_set_position(const servo_handle_t *hsrv, int32_t pos);
+
 #endif /* DRIVER_DEVICE_SERVO_H_ */
