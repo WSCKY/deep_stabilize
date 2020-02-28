6d34c75393cbed39ac33c4058c3afad25742ca97
kyChu <hello.kychu@gmail.com>
2020-02-29 00:47:26 +0800

    apply log function

--- ctrl/apps/main_task.c
+++ ctrl/apps/main_task.c
@@ -13,6 +13,8 @@
 #include "usbd_cdc_vcp.h"
 #endif /* CONFIG_USB_IF_ENABLE */
 
+static const char* TAG = "MAIN";
+
 static void error_handler(int code);
 
 /**
@@ -30,7 +32,20 @@ void StartThread(void const * arg)
   board_gpio_init();
 /*  pwm_init(); */
 
+  if(uart2_init() != status_ok) {
+    error_handler(1);
+  }
+
+#if CONFIG_LOG_ENABLE
+  if(log_init(uart2_TxString) != status_ok) {
+    error_handler(1);
+  }
+#endif /* CONFIG_LOG_ENABLE */
+
   if(rs485_init() != status_ok) {
+#if CONFIG_LOG_ENABLE
+    ky_err(TAG, "rs485 init failed.");
+#endif /* CONFIG_LOG_ENABLE */
     error_handler(1);
   }
   delay(50);
@@ -41,6 +56,9 @@ void StartThread(void const * arg)
      pins and interrupt service routine (BSP) to start the Library*/
   USB_Device_dev = kmm_alloc(sizeof(USB_CORE_HANDLE));
   if(USB_Device_dev == NULL) {
+#if CONFIG_LOG_ENABLE
+    ky_err(TAG, "usb init failed.");
+#endif /* CONFIG_LOG_ENABLE */
     error_handler(5);
   }
   USBD_Init(USB_Device_dev, &USR_desc, &USBD_CDC_cb, &USR_cb);
@@ -49,18 +67,31 @@ void StartThread(void const * arg)
 
   osThreadDef(T_SINS, sins_task, osPriorityNormal, 0, configMINIMAL_STACK_SIZE);
   if(osThreadCreate (osThread(T_SINS), NULL) == NULL) {
+#if CONFIG_LOG_ENABLE
+    ky_err(TAG, "sins task create failed.");
+#endif /* CONFIG_LOG_ENABLE */
     error_handler(2);
   }
 
-  osThreadDef(T_CTRL, ctrl_task, osPriorityNormal, 0, configMINIMAL_STACK_SIZE);
+  osThreadDef(T_CTRL, ctrl_task, osPriorityNormal, 0, configMINIMAL_STACK_SIZE * 2);
   if(osThreadCreate (osThread(T_CTRL), NULL) == NULL) {
+#if CONFIG_LOG_ENABLE
+    ky_err(TAG, "ctrl task create failed.");
+#endif /* CONFIG_LOG_ENABLE */
     error_handler(3);
   }
 
   osThreadDef(T_COM, com_task, osPriorityNormal, 0, configMINIMAL_STACK_SIZE);
   if(osThreadCreate (osThread(T_COM), NULL) == NULL) {
+#if CONFIG_LOG_ENABLE
+    ky_err(TAG, "mesg task create failed.");
+#endif /* CONFIG_LOG_ENABLE */
     error_handler(4);
   }
+
+#if CONFIG_LOG_ENABLE
+    ky_info(TAG, "application started!");
+#endif /* CONFIG_LOG_ENABLE */
 
 /*  mpu9250_init();
   _delay_ms(10); */
