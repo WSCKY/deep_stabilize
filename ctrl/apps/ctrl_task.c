29a7544ee05de31b2e867024643d5e85eedadc96
kyChu <hello.kychu@gmail.com>
2020-03-03 10:17:58 +0800

    update ctrl task

--- ctrl/apps/ctrl_task.c
+++ ctrl/apps/ctrl_task.c
@@ -8,46 +8,49 @@
 #include "main_task.h"
 
 #include "servo.h"
+#include <math.h>
 
-static const char* TAG = "CTRL";
+#if CONFIG_LOG_ENABLE
+//static const char* TAG = "CTRL";
+#endif /* CONFIG_LOG_ENABLE */
 
 //float exp_angle = 0.0f;
-//extern AngleInfo_t AngleInfo;
+extern AngleInfo_t AngleInfo;
 
-extern rtu_handle_t rtu_rs485_2;
+/* extern rtu_handle_t rtu_rs485_1;
 
 static const servo_handle_t motor_pitch = {
   .addr = 0x01,
-  .hrtu = &rtu_rs485_2,
-};
+  .hrtu = &rtu_rs485_1,
+}; */
 
 void ctrl_task(void const *arg)
 {
-//  uint32_t task_period_div = 0;
+  uint32_t task_period_div = 0;
 
-  for(;;) {
-  delay(1000);
-//  servo_set_current(&motor_pitch, 2);
-//  delay(1000);
-#if CONFIG_LOG_ENABLE
-    ky_info(TAG, "servo run 5000 pulse.");
-#endif /* CONFIG_LOG_ENABLE */
-  servo_run_pulse(&motor_pitch, 5000);
-//  delay(1000);
+  delay(1000); // wait motor driver ready.
+
+  if(tim7_init() != status_ok) {
+    vTaskDelete(NULL);
   }
 
-//  if(tim7_init() != status_ok) {
-//    vTaskDelete(NULL);
-//  }
-//
-//  for(;;) {
-//    if(tim7_check_update(100) == status_ok) {
-//      task_period_div ++;
-//      if(task_period_div == 4) {
-//        task_period_div = 0;
-//
-////      (AngleInfo.AngleVal - exp_angle) * 50 * 200 / 360
-//      }
-//    }
-//  }
+  pwm2_period(0);
+
+  for(;;) {
+    if(tim7_check_update(100) == status_ok) {
+      task_period_div ++;
+      if(task_period_div == 4) { // 20ms
+        task_period_div = 0;
+
+        pwm2_period((int)(25 * fabs((AngleInfo.AngleVal - 0))));
+        if(AngleInfo.AngleVal < 0) {
+        	output_port_clear(IO_OUTPUT2);
+        } else {
+        	output_port_set(IO_OUTPUT2);
+        }
+//        ky_info(TAG, "loop");
+//      (AngleInfo.AngleVal - exp_angle) * 50 * 200 / 360
+      }
+    }
+  }
 }
