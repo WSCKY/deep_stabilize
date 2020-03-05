3679f87f1e9f04746a3168d11e072b911eefdc9d
kyChu <hello.kychu@gmail.com>
2020-03-05 10:10:36 +0800

    add stat_task module

--- ctrl/apps/subdir.mk
+++ ctrl/apps/subdir.mk
@@ -8,6 +8,7 @@ C_SRCS += \
 ./apps/main_task.c \
 ./apps/sins_task.c \
 ./apps/ctrl_task.c \
+./apps/stat_task.o \
 ./apps/com_task.o \
 ./apps/AttEst.c \
 ./apps/printHardFault.c
@@ -17,6 +18,7 @@ OBJS += \
 $(BuildPath)/apps/main_task.o \
 $(BuildPath)/apps/sins_task.o \
 $(BuildPath)/apps/ctrl_task.o \
+$(BuildPath)/apps/stat_task.o \
 $(BuildPath)/apps/com_task.o \
 $(BuildPath)/apps/AttEst.o \
 $(BuildPath)/apps/printHardFault.o
@@ -26,6 +28,7 @@ C_DEPS += \
 $(BuildPath)/apps/main_task.d \
 $(BuildPath)/apps/sins_task.d \
 $(BuildPath)/apps/ctrl_task.d \
+$(BuildPath)/apps/stat_task.d \
 $(BuildPath)/apps/com_task.d \
 $(BuildPath)/apps/AttEst.d \
 $(BuildPath)/apps/printHardFault.d
