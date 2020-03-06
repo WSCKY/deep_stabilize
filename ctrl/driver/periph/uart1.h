a5a6cc32758310e822d497ad61ff8693a0d5880d
kyChu <hello.kychu@gmail.com>
2020-03-06 17:34:23 +0800

    update to support DMA

--- ctrl/driver/periph/uart1.h
+++ ctrl/driver/periph/uart1.h
@@ -10,6 +10,8 @@
 
 #include "SysConfig.h"
 
+#define UART1_DMA_ENABLE                    (1)
+
 #define UART1                               USART1
 #define UART1_GPIO                          GPIOB
 #define UART1_AF                            GPIO_AF_0
@@ -22,18 +24,41 @@
 #define UART1_TxPin_AF                      GPIO_PinSource6
 /* #define UART1_IRQn                          USART1_IRQn */
 
+#if UART1_DMA_ENABLE
+#define UART1_TX_DMA                        DMA1_Channel2
+#define UART1_RX_DMA                        DMA1_Channel3
+#define UART1_TDR_ADDRESS                   0x40013828
+#define UART1_RDR_ADDRESS                   0x40013824
+#define UART1_DMA_CLK                       RCC_AHBPeriph_DMA1
+#define UART1_DMA_CLK_CMD                   RCC_AHBPeriphClockCmd
+#define UART1_TX_DMA_IT_TC_FLAG             DMA1_IT_TC2
+#define UART1_RX_DMA_IT_HT_FLAG             DMA1_IT_HT3
+#define UART1_RX_DMA_IT_TC_FLAG             DMA1_IT_TC3
+/* #define UART1_DMA_IRQn                      DMA1_Channel2_3_IRQn */ /*!< DMA1 Channel 2, Channel 3 Interrupts */
+#endif /* UART1_DMA_ENABLE */
+
 #define UART1_RX_CACHE_SIZE                 80
 
 status_t uart1_init(uint32_t baudrate);
 
 void uart1_TxByte(uint8_t c);
 void uart1_TxBytes(uint8_t *p, uint32_t l);
-
+#if UART1_DMA_ENABLE
+status_t uart1_TxBytesDMA(uint8_t *p, uint32_t l);
+#if FREERTOS_ENABLED
+status_t uart1_TxBytesDMA_Block(uint8_t *p, uint32_t l, uint32_t timeout);
+#endif /* FREERTOS_ENABLED */
+#endif /* UART1_DMA_ENABLE */
 uint8_t uart1_pullByte(uint8_t *p);
 uint32_t uart1_pullBytes(uint8_t *p, uint32_t l);
 
 status_t uart1_waitBytes(uint32_t size, uint32_t timeout);
+#if FREERTOS_ENABLED
+status_t uart1_read_block(uint8_t *p, uint32_t l, uint32_t timeout);
+#endif /* FREERTOS_ENABLED */
 
+/* interrupt handler */
 void uart1_irq_handler(void);
+void uart1_dma_irq_handler(void);
 
 #endif /* __UART1_H_ */
