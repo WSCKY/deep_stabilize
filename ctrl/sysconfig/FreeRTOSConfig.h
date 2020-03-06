413fddeb1928dd7e5cc9eb40d08be175b71703bb
kyChu <hello.kychu@gmail.com>
2020-03-06 17:33:35 +0800

    update RTOS configuration

--- ctrl/sysconfig/FreeRTOSConfig.h
+++ ctrl/sysconfig/FreeRTOSConfig.h
@@ -97,25 +97,26 @@
 #define configCPU_CLOCK_HZ			( SystemCoreClock )
 #define configTICK_RATE_HZ			( ( TickType_t ) 1000 )
 #define configMAX_PRIORITIES			(  7 )
-#define configMINIMAL_STACK_SIZE		( ( uint16_t ) 64 )
-#if CONFIG_USB_IF_ENABLE
- #define configTOTAL_HEAP_SIZE			( ( size_t ) ( 4 * 1024 ) )
+#define configMINIMAL_STACK_SIZE		( ( uint16_t ) 128 )
+#if defined(FREERTOS_HEAP_SIZE)
+ #define configTOTAL_HEAP_SIZE			( ( size_t ) FREERTOS_HEAP_SIZE )
+ #define configAPPLICATION_ALLOCATED_HEAP        1
 #else
- #define configTOTAL_HEAP_SIZE			( ( size_t ) ( 5 * 1024 ) )
-#endif /* CONFIG_USB_IF_ENABLE */
+ #define configTOTAL_HEAP_SIZE           ( ( size_t ) (4 * 1024) )
+#endif /* FREERTOS_HEAP_SIZE */
 #define configMAX_TASK_NAME_LEN			( 16 )
-#define configUSE_TRACE_FACILITY		1
+#define configUSE_TRACE_FACILITY		0
 #define configUSE_16_BIT_TICKS			0
 #define configIDLE_SHOULD_YIELD			1
 #define configUSE_MUTEXES			1
 #define configQUEUE_REGISTRY_SIZE		8
-#define configCHECK_FOR_STACK_OVERFLOW	        0
+#define configCHECK_FOR_STACK_OVERFLOW	        1
 #define configUSE_RECURSIVE_MUTEXES		1
 #define configUSE_MALLOC_FAILED_HOOK	        0
 #define configUSE_APPLICATION_TASK_TAG	        0
 #define configUSE_COUNTING_SEMAPHORES	        1
 #define configGENERATE_RUN_TIME_STATS	        0
-#define configUSE_STATS_FORMATTING_FUNCTIONS    1
+#define configUSE_STATS_FORMATTING_FUNCTIONS    0
 
 /* Co-routine definitions. */
 #define configUSE_CO_ROUTINES 		        0
