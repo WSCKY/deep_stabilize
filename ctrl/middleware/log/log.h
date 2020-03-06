6bf8d6a63c14ffb81ecc699a23d9975d4611717c
kyChu <hello.kychu@gmail.com>
2020-03-06 17:30:09 +0800

    update definitions

--- ctrl/middleware/log/log.h
+++ ctrl/middleware/log/log.h
@@ -9,17 +9,13 @@
 #define _LOG_H_
 
 #include "logconfig.h"
-#include "SysConfig.h"
 
-#define DEBUG_ENABLE                             (1)
-#define CONFIG_LOG_COLORS                        (1)
-
-#ifdef DEBUG_ENABLE
+#if CONFIG_LOG_OUTPUT_ENABLED
 #define CONFIG_DEBUG_ALERT
 #define CONFIG_DEBUG_ERROR
 #define CONFIG_DEBUG_WARN
 #define CONFIG_DEBUG_INFO
-#endif /* defined(DEBUG_ENABLE) */
+#endif /* CONFIG_LOG_OUTPUT_ENABLED */
 
 typedef enum {
     KY_LOG_NONE,       /*!< No log output */
@@ -32,13 +28,13 @@ typedef enum {
 
 typedef status_t (*log_put_t)(const char *);
 
-#ifdef DEBUG_ENABLE
+#if CONFIG_LOG_OUTPUT_ENABLED
 status_t log_init(log_put_t ptx);
 uint32_t log_timestamp(void);
 status_t log_write(const char *format, ...) __attribute__ ((__format__ (__printf__, 1, 2)));
-#endif /* defined(DEBUG_ENABLE) */
+#endif /* CONFIG_LOG_OUTPUT_ENABLED */
 
-#if CONFIG_LOG_COLORS
+#if CONFIG_LOG_COLORS_ENABLED
 #define LOG_COLOR_BLACK   "30"
 #define LOG_COLOR_RED     "31"
 #define LOG_COLOR_GREEN   "32"
@@ -54,14 +50,14 @@ status_t log_write(const char *format, ...) __attribute__ ((__format__ (__printf
 #define LOG_COLOR_I       LOG_COLOR(LOG_COLOR_GREEN)
 #define LOG_COLOR_D
 #define LOG_COLOR_V
-#else //CONFIG_LOG_COLORS
+#else //CONFIG_LOG_COLORS_ENABLED
 #define LOG_COLOR_E
 #define LOG_COLOR_W
 #define LOG_COLOR_I
 #define LOG_COLOR_D
 #define LOG_COLOR_V
 #define LOG_RESET_COLOR
-#endif //CONFIG_LOG_COLORS
+#endif //CONFIG_LOG_COLORS_ENABLED
 
 #define LOG_FORMAT(letter, format)  LOG_COLOR_ ## letter #letter " (%ld) %s: " format LOG_RESET_COLOR "\n"
 
