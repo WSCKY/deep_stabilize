49db73688c071e4ea9893c9b3ce1a3dd8dddd1a9
kyChu <hello.kychu@gmail.com>
2020-03-06 17:37:28 +0800

    add new function for DMA support

--- ctrl/driver/periph/uart1.c
+++ ctrl/driver/periph/uart1.c
@@ -13,9 +13,20 @@
 
 static uint8_t _uart1_init_flag = 0;
 
+#if UART1_DMA_ENABLE
+static uint32_t _tx_comp_flag = 1;
+#if FREERTOS_ENABLED
+/* Define used semaphore */
+osSemaphoreDef(U1_TX_SEM);
+static osSemaphoreId u1_tx_semaphore;
+#endif /* FREERTOS_ENABLED */
+#endif /* UART1_DMA_ENABLE */
+
 static uint32_t subscribe_number = 0;
 #if FREERTOS_ENABLED
-static osSemaphoreId u1_semaphore;
+/* Define used semaphore */
+osSemaphoreDef(U1_RX_SEM);
+static osSemaphoreId u1_rx_semaphore;
 #endif /* FREERTOS_ENABLED */
 
 static uint32_t in_ptr = 0, out_ptr = 0;
@@ -24,6 +35,10 @@ uint8_t *UART1_RX_CACHE;
 #else
 uint8_t UART1_RX_CACHE[UART1_RX_CACHE_SIZE];
 #endif /* FREERTOS_ENABLED */
+
+#if UART1_DMA_ENABLE
+static void uart1_dma_config(void);
+#endif /* UART1_DMA_ENABLE */
 
 status_t uart1_init(uint32_t baudrate)
 {
@@ -36,11 +51,15 @@ status_t uart1_init(uint32_t baudrate)
 #if FREERTOS_ENABLED
   UART1_RX_CACHE = kmm_alloc(UART1_RX_CACHE_SIZE);
   if(UART1_RX_CACHE == NULL) return status_nomem;
-  /* Define used semaphore */
-  osSemaphoreDef(U1_SEM);
+#if UART1_DMA_ENABLE
   /* Create the semaphore */
-  u1_semaphore = osSemaphoreCreate(osSemaphore(U1_SEM) , 1);
-  if(u1_semaphore == NULL) return status_error;
+  u1_tx_semaphore = osSemaphoreCreate(osSemaphore(U1_TX_SEM) , 1);
+  if(u1_tx_semaphore == NULL) return status_error;
+  osSemaphoreWait(u1_tx_semaphore, 0);
+#endif /* UART1_DMA_ENABLE */
+  /* Create the semaphore */
+  u1_rx_semaphore = osSemaphoreCreate(osSemaphore(U1_RX_SEM) , 1);
+  if(u1_rx_semaphore == NULL) return status_error;
 #endif /* FREERTOS_ENABLED */
 
   /* Enable GPIO clock */
@@ -64,6 +83,10 @@ status_t uart1_init(uint32_t baudrate)
 
   /* Enable UART1 Clock */
   UART1_CLK_CMD(UART1_CLK, ENABLE);
+
+#if UART1_DMA_ENABLE
+  uart1_dma_config();
+#endif /* UART1_DMA_ENABLE */
 
   /* UART1 configuration -------------------------------------------*/
   /* UART1 configured as follow:
@@ -91,8 +114,15 @@ status_t uart1_init(uint32_t baudrate)
   NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
   NVIC_Init(&NVIC_InitStructure); */
 
+#if UART1_DMA_ENABLE
+  /* Enable the UART1 Idle line detection interrupt. */
+  USART_ITConfig(UART1, USART_IT_IDLE, ENABLE);
+  /* Enable the UART1 Tx DMA requests */
+  USART_DMACmd(UART1, USART_DMAReq_Tx | USART_DMAReq_Rx, ENABLE);
+#else
   /* Enable the UART1 Receive Interrupt */
   USART_ITConfig(UART1, USART_IT_RXNE, ENABLE);
+#endif /* UART1_DMA_ENABLE */
 
   /* Enable UART1 */
   USART_Cmd(UART1, ENABLE);
@@ -100,6 +130,62 @@ status_t uart1_init(uint32_t baudrate)
   _uart1_init_flag = 1;
   return status_ok;
 }
+
+#if UART1_DMA_ENABLE
+static void uart1_dma_config(void)
+{
+  DMA_InitTypeDef DMA_InitStructure;
+/*	NVIC_InitTypeDef NVIC_InitStructure; */
+
+  /* Enable UART1_DMA Clock */
+  UART1_DMA_CLK_CMD(UART1_DMA_CLK, ENABLE);
+
+  DMA_DeInit(UART1_TX_DMA);
+  DMA_Cmd(UART1_TX_DMA, DISABLE);
+  DMA_DeInit(UART1_RX_DMA);
+  DMA_Cmd(UART1_RX_DMA, DISABLE);
+
+  /* UART1_RX_DMA configuration */
+  DMA_InitStructure.DMA_PeripheralDataSize = DMA_PeripheralDataSize_Byte;
+  DMA_InitStructure.DMA_MemoryDataSize = DMA_MemoryDataSize_Byte;
+  DMA_InitStructure.DMA_PeripheralInc = DMA_PeripheralInc_Disable;
+  DMA_InitStructure.DMA_MemoryInc = DMA_MemoryInc_Enable;
+  DMA_InitStructure.DMA_M2M = DMA_M2M_Disable;
+
+  DMA_InitStructure.DMA_BufferSize = UART1_RX_CACHE_SIZE;
+  DMA_InitStructure.DMA_Mode = DMA_Mode_Circular;
+  DMA_InitStructure.DMA_MemoryBaseAddr = (uint32_t)UART1_RX_CACHE;
+  DMA_InitStructure.DMA_DIR = DMA_DIR_PeripheralSRC;
+  DMA_InitStructure.DMA_Priority = DMA_Priority_High;
+  DMA_InitStructure.DMA_PeripheralBaseAddr = UART1_RDR_ADDRESS;
+  DMA_Init(UART1_RX_DMA, &DMA_InitStructure);
+
+  /* UART1_TX_DMA configuration */
+  DMA_InitStructure.DMA_BufferSize = 0;
+  DMA_InitStructure.DMA_Mode = DMA_Mode_Normal;
+  DMA_InitStructure.DMA_MemoryBaseAddr = 0;
+  DMA_InitStructure.DMA_DIR = DMA_DIR_PeripheralDST;
+  DMA_InitStructure.DMA_Priority = DMA_Priority_Low;
+  DMA_InitStructure.DMA_PeripheralBaseAddr = UART1_TDR_ADDRESS;
+  DMA_Init(UART1_TX_DMA, &DMA_InitStructure);
+
+  /* Enable the UART1_TX_DMA TC Interrupt */
+  DMA_ITConfig(UART1_TX_DMA, DMA_IT_TC, ENABLE);
+  /* Enable the UART1_RX_DMA HT/TC Interrupt */
+  DMA_ITConfig(UART1_RX_DMA, DMA_IT_HT, ENABLE);
+  DMA_ITConfig(UART1_RX_DMA, DMA_IT_TC, ENABLE);
+
+/*  NVIC_InitStructure.NVIC_IRQChannel = UART1_DMA_IRQn;
+  NVIC_InitStructure.NVIC_IRQChannelPriority = UART1_DMA_INT_PRIORITY;
+  NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+  NVIC_Init(&NVIC_InitStructure); */
+
+  /* Enable the UART1_TX_DMA channels */
+//  DMA_Cmd(UART1_TX_DMA, ENABLE);
+  /* Enable the UART1_RX_DMA channels */
+  DMA_Cmd(UART1_RX_DMA, ENABLE);
+}
+#endif /* UART1_DMA_ENABLE */
 
 void uart1_TxByte(uint8_t c)
 {
@@ -116,6 +202,42 @@ void uart1_TxBytes(uint8_t *p, uint32_t l)
   }
 }
 
+#if UART1_DMA_ENABLE
+status_t uart1_TxBytesDMA(uint8_t *p, uint32_t l)
+{
+  if(_tx_comp_flag == 1) {
+    _tx_comp_flag = 0;
+    UART1_TX_DMA->CNDTR = l;
+    UART1_TX_DMA->CMAR = (uint32_t)p;
+
+    /* Enable the UART1_DMA channels */
+    UART1_TX_DMA->CCR |= DMA_CCR_EN;
+    return status_ok;
+  }
+  return status_busy;
+}
+
+#if FREERTOS_ENABLED
+status_t uart1_TxBytesDMA_Block(uint8_t *p, uint32_t l, uint32_t timeout)
+{
+  if(_tx_comp_flag == 1) {
+    _tx_comp_flag = 0;
+    UART1_TX_DMA->CNDTR = l;
+    UART1_TX_DMA->CMAR = (uint32_t)p;
+
+    /* Enable the UART1_DMA channels */
+    UART1_TX_DMA->CCR |= DMA_CCR_EN;
+//    osSemaphoreWait(u1_tx_semaphore, 0);
+    if(osSemaphoreWait(u1_tx_semaphore, timeout) == osOK)
+      return status_ok;
+    else
+      return status_error;
+  }
+  return status_busy;
+}
+#endif /* FREERTOS_ENABLED */
+#endif /* UART1_DMA_ENABLE */
+
 status_t uart1_waitBytes(uint32_t size, uint32_t timeout)
 {
   if(UART1_RX_CACHE_SIZE < size)
@@ -123,7 +245,7 @@ status_t uart1_waitBytes(uint32_t size, uint32_t timeout)
   else
     subscribe_number = size;
 #if FREERTOS_ENABLED
-  if(osSemaphoreWait(u1_semaphore , timeout) == osOK) {
+  if(osSemaphoreWait(u1_rx_semaphore , timeout) == osOK) {
     return status_ok;
   }
   return status_timeout;
@@ -166,6 +288,55 @@ uint32_t uart1_pullBytes(uint8_t *p, uint32_t l)
   return len;
 }
 
+#if FREERTOS_ENABLED
+status_t uart1_read_block(uint8_t *p, uint32_t l, uint32_t timeout)
+{
+  uint32_t num_rd;
+  uint32_t ts_now, ts_start;
+  ts_start = xTaskGetTickCount() * (1000 / configTICK_RATE_HZ);
+
+read:
+  num_rd = uart1_pullBytes(p, l);
+  p += num_rd;
+  if(num_rd < l) {
+    l -= num_rd;
+    if(UART1_RX_CACHE_SIZE < l) subscribe_number = UART1_RX_CACHE_SIZE;
+    else subscribe_number = l;
+    ts_now = xTaskGetTickCount() * (1000 / configTICK_RATE_HZ);
+    if((ts_now - ts_start) < timeout) {
+      if(osSemaphoreWait(u1_rx_semaphore , timeout + ts_start - ts_now) == osOK) {
+        goto read;
+      }
+    }
+    return status_timeout;
+  }
+  return status_ok;
+}
+#endif /* FREERTOS_ENABLED */
+
+#if UART1_DMA_ENABLE
+static void uart1_pushBytes(void)
+{
+  uint32_t len;
+  /* Calculate current position in buffer */
+  in_ptr = UART1_RX_CACHE_SIZE - UART1_RX_DMA->CNDTR;
+  if(subscribe_number != 0) {
+    if(in_ptr > out_ptr) {
+      len = in_ptr - out_ptr;
+    } else {
+      len = UART1_RX_CACHE_SIZE - out_ptr + in_ptr;
+    }
+    if(len >= subscribe_number) {
+      subscribe_number = 0; // clear flag
+#if FREERTOS_ENABLED
+      osSemaphoreRelease(u1_rx_semaphore);
+#else
+      // callback
+#endif /* FREERTOS_ENABLED */
+    }
+  }
+}
+#else
 static void uart1_pushByte(uint8_t byte)
 {
   uint32_t len;
@@ -182,22 +353,58 @@ static void uart1_pushByte(uint8_t byte)
     }
     if(len >= subscribe_number) {
 #if FREERTOS_ENABLED
-      osSemaphoreRelease(u1_semaphore);
+      osSemaphoreRelease(u1_rx_semaphore);
 #else
       // callback
 #endif /* FREERTOS_ENABLED */
     }
   }
 }
+#endif /* UART1_DMA_ENABLE */
+
+#if UART1_DMA_ENABLE
+void uart1_dma_irq_handler(void)
+{
+  if(_uart1_init_flag == 0) return;
+  /* check if transfer complete flag is set. */
+  if(DMA_GetITStatus(UART1_TX_DMA_IT_TC_FLAG)) {
+    DMA_Cmd(UART1_TX_DMA, DISABLE);
+    _tx_comp_flag = 1;
+#if FREERTOS_ENABLED
+    osSemaphoreRelease(u1_tx_semaphore);
+#endif /* FREERTOS_ENABLED */
+    DMA_ClearITPendingBit(UART1_TX_DMA_IT_TC_FLAG);
+  }
+  /* check if receive half complete flag is set. */
+  if(DMA_GetITStatus(UART1_RX_DMA_IT_HT_FLAG)) {
+    uart1_pushBytes();
+    DMA_ClearITPendingBit(UART1_RX_DMA_IT_HT_FLAG);
+  }
+  /* check if receive complete flag is set. */
+  if(DMA_GetITStatus(UART1_RX_DMA_IT_TC_FLAG)) {
+    uart1_pushBytes();
+    DMA_ClearITPendingBit(UART1_RX_DMA_IT_TC_FLAG);
+  }
+}
+#endif /* UART1_DMA_ENABLE */
 
 void uart1_irq_handler(void)
 {
   if(_uart1_init_flag == 0) return;
-
+#if UART1_DMA_ENABLE
+  if(USART_GetITStatus(UART1, USART_IT_IDLE) != RESET) {
+    uart1_pushBytes();
+    USART_ClearFlag(UART1, USART_FLAG_IDLE);
+    USART_ClearITPendingBit(UART1, USART_IT_IDLE);
+  }
+#else
   USART_ClearFlag(UART1, USART_FLAG_ORE | USART_FLAG_PE);
   if(USART_GetITStatus(UART1, USART_IT_RXNE) != RESET) {
     uart1_pushByte(UART1->RDR & 0xFF);
     USART_ClearFlag(UART1, USART_FLAG_RXNE);
     USART_ClearITPendingBit(UART1, USART_IT_RXNE);
   }
+#endif /* UART1_DMA_ENABLE */
 }
+
+/******************** kyChu<kyChu@qq.com> **** END OF FILE ********************/
