c01b9ec5f34cf484cbf6a7070ad51f1398c0611f
kyChu <hello.kychu@gmail.com>
2020-03-06 17:39:15 +0800

    add function prototype

--- ctrl/driver/device/rs485.h
+++ ctrl/driver/device/rs485.h
@@ -10,6 +10,13 @@
 
 #include "ifconfig.h"
 
-status_t rs485_init(void);
+status_t rs485_1_init(void);
+status_t rs485_1_tx(uint8_t *buffer, uint32_t size);
+status_t rs485_1_rx(uint8_t *buffer, uint32_t size);
+
+#if CONFIG_RS485_2_ENABLE
+status_t rs485_2_tx(uint8_t *buffer, uint32_t size);
+status_t rs485_2_rx(uint8_t *buffer, uint32_t size, uint32_t timeout);
+#endif /* CONFIG_RS485_2_ENABLE */
 
 #endif /* DRIVER_DEVICE_RS485_H_ */
