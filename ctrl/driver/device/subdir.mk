4b6c9fbaca80babc105f1b630d85932cd7945ad1
kyChu <hello.kychu@gmail.com>
2020-03-06 17:31:34 +0800

    compile mpu9250 driver module if 'CONFIG_USE_BOARD_IMU' defined only

--- ctrl/driver/device/subdir.mk
+++ ctrl/driver/device/subdir.mk
@@ -5,22 +5,27 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
-./driver/device/mpu9250.c \
 ./driver/device/rs485.c \
 ./driver/device/servo.c \
 ./driver/device/encoder.c
 
 OBJS += \
-$(BuildPath)/driver/device/mpu9250.o \
 $(BuildPath)/driver/device/rs485.o \
 $(BuildPath)/driver/device/servo.o \
 $(BuildPath)/driver/device/encoder.o
 
 C_DEPS += \
-$(BuildPath)/driver/device/mpu9250.d \
 $(BuildPath)/driver/device/rs485.d \
 $(BuildPath)/driver/device/servo.d \
 $(BuildPath)/driver/device/encoder.d
+
+ifeq ($(CONFIG_USE_BOARD_IMU),y)
+C_SRCS += ./driver/device/mpu9250.c
+
+OBJS += $(BuildPath)/driver/device/mpu9250.o
+
+C_DEPS += $(BuildPath)/driver/device/mpu9250.d
+endif
 
 OBJ_DIRS = $(sort $(dir $(OBJS)))
 
