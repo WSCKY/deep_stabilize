fd5d582d2af598b8dd854d1df6070e6f507b370c
kyChu <hello.kychu@gmail.com>
2020-03-07 17:53:06 +0800

    get arguments from parameter list

--- ctrl/apps/ctrl_task.c
+++ ctrl/apps/ctrl_task.c
@@ -6,6 +6,7 @@
  */
 
 #include "main_task.h"
+#include "parameter.h"
 #include "maths.h"
 #include "servo.h"
 #include <math.h>
@@ -17,7 +18,6 @@
 #define OUTPUT_LIMIT           8000
 
 //float exp_angle = 0.0f;
-extern AngleInfo_t AngleInfo;
 
 /* extern rtu_handle_t rtu_rs485_1;
 
@@ -29,7 +29,8 @@ static const servo_handle_t motor_pitch = {
 void ctrl_task(void const *arg)
 {
   PID *pid;
-  float angle = 0;
+  AngleInfo_t AngleInfo;
+  float angle = 0, exp_ang;
 //  uint32_t task_period_div = 0;
   pwm16_init();
   pwm17_init();
@@ -50,21 +51,24 @@ void ctrl_task(void const *arg)
 
   for(;;) {
     if(tim7_check_update(100) == status_ok) {
-        if(ABS(0 - AngleInfo.AngleVal) < 0.5f)
-        	angle = 0.0f;
-        else
-        	angle = AngleInfo.AngleVal;
-        pid_loop(pid, 0, angle);
+      param_get_anginfo(&AngleInfo);
+      param_get_exppit(&exp_ang);
+
+      if(ABS(exp_ang - AngleInfo.AngleVal) < 0.5f)
+        angle = exp_ang;
+      else
+        angle = AngleInfo.AngleVal;
+      pid_loop(pid, exp_ang, angle);
 //        pid->Output += ((0 - angle) * 300 - AngleInfo.AngleRateVal) * 1.0f;
-        pid->Output = pid->Output - AngleInfo.AngleRateVal * 40.6667f;
-        pid->Output = LIMIT(pid->Output, OUTPUT_LIMIT, -OUTPUT_LIMIT);
-        pwm16_period((uint32_t)ABS(pid->Output));
+      pid->Output = pid->Output - AngleInfo.AngleRateVal * 40.6667f;
+      pid->Output = LIMIT(pid->Output, OUTPUT_LIMIT, -OUTPUT_LIMIT);
+      pwm16_period((uint32_t)ABS(pid->Output));
 //        pwm16_period((int)(1200 * fabs((AngleInfo.AngleVal - 0))));
-        if(pid->Output < 0) {
-          output_port_set(IO_OUTPUT2);
-        } else {
-          output_port_clear(IO_OUTPUT2);
-        }
+      if(pid->Output < 0) {
+        output_port_set(IO_OUTPUT2);
+      } else {
+        output_port_clear(IO_OUTPUT2);
+      }
     }
   }
 }
