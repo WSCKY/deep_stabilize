690ac056a6151c02b65a642ec5bd498be147453e
kyChu <hello.kychu@gmail.com>
2020-03-18 17:09:59 +0800

    update intio driver

--- ctrl/driver/periph/intio.h
+++ ctrl/driver/periph/intio.h
@@ -17,11 +17,13 @@
 #define INTx_GPIO_PinSource                 EXTI_PinSource13
 #define INTx_EXTI_LINE                      EXTI_Line13
 #define INTx_EXTI_IRQn                      EXTI4_15_IRQn
-#define INTx_EXTI_IRQHandler                EXTI4_15_IRQHandler
+/* #define INTx_EXTI_IRQHandler                EXTI4_15_IRQHandler */
 
 typedef void (*INTIO_IRQCallback)(void);
 
 void intio_init(void);
 void intio_set_irq_handler(INTIO_IRQCallback p);
+
+void intio_irq_handler(void);
 
 #endif /* DRIVER_PERIPH_INTIO_H_ */
