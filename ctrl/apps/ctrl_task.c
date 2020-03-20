b12aa73650c518d62edd3c621aa4df6be6365681
kyChu <hello.kychu@gmail.com>
2020-03-20 09:49:13 +0800

    disable stabilize control mode

--- ctrl/apps/ctrl_task.c
+++ ctrl/apps/ctrl_task.c
@@ -16,29 +16,42 @@
 static const char* TAG = "CTRL";
 #endif /* CONFIG_LOG_ENABLE */
 
-#define CTRL_LOOP_PERIOD_MS        (2)           /* 2ms */
+/*
+ * keep ctrl_task task handle
+ */
+static osThreadId ctrlThread;
 
-#define PITCH_ADJ_ANGLE_RATE       (20)          /* 20deg/s */
-#define PITCH_ADJ_ANGLE_DEADBAND   (0.5f)        /* +/- 0.5deg */
+/*
+ * task notify callback
+ */
+static void ctrl_task_notify(void);
+
+/* pitch angle limitation */
 #define PITCH_ANGLE_LIMIT_MIN      (-15.0f)      /* down: -15deg */
 #define PITCH_ANGLE_LIMIT_MAX      (30.0f)       /* up: 30deg */
-
-#define YAW_ADJ_ANGLE_RATE         (60)          /* 60deg/s */
+/* yaw angle limitation */
 #define YAW_ANGLE_LIMIT_MIN        (-90.0f)      /* right: -90deg */
 #define YAW_ANGLE_LIMIT_MAX        (90.0f)       /* left: 90deg */
-
+/* encoder ID */
 #define PITCH_MOTOR_ENCODER_ID     (0)           /* encoder 0 */
-#define PITCH_MOTOR_OUTPUT_LIMIT   (8000)        /* 8KHz */
-
 #define YAW_MOTOR_ENCODER_ID       (1)           /* encoder 1 */
-#define YAW_MOTOR_OUTPUT_LIMIT     (10000)       /* 10KHz */
+/* motor output limitation */
+#define PITCH_MOTOR_OUTPUT_LIMIT   (8000)        /* 8KHz */
+#define YAW_MOTOR_OUTPUT_LIMIT     (8000)        /* 8KHz */
+
+#define CTRL_STABILIZE_MODE_ENABLE (0)           /* enable(1) / disable(0) stabilize control mode */
+
+#if CTRL_STABILIZE_MODE_ENABLE
+
+#define CTRL_LOOP_PERIOD_MS        (2)           /* 2ms */
+
+#define PITCH_ADJ_ANGLE_RATE       (10)          /* 10deg/s */
+#define PITCH_ADJ_ANGLE_DEADBAND   (0.5f)        /* +/- 0.5deg */
+
+#define YAW_ADJ_ANGLE_RATE         (60)          /* 60deg/s */
 
 #define PITCH_ADJ_STEP_DEG         (PITCH_ADJ_ANGLE_RATE * CTRL_LOOP_PERIOD_MS * 0.001f)
 #define YAW_ADJ_STEP_DEG           (YAW_ADJ_ANGLE_RATE * CTRL_LOOP_PERIOD_MS * 0.001f)
-
-static osThreadId ctrlThread;
-
-static void ctrl_task_notify(void);
 
 void ctrl_task(void const *arg)
 {
@@ -70,7 +83,7 @@ void ctrl_task(void const *arg)
   pwm17_init();
   delay(1000); // wait motor driver ready.
 
-  pid->kp = 2500;
+  pid->kp = 100;
   pid->ki = 000;
   pid->kd = 0;
   pid->I_max = 3000;
@@ -91,10 +104,11 @@ void ctrl_task(void const *arg)
         exp_angle = LIMIT(exp_angle, PITCH_ANGLE_LIMIT_MAX, PITCH_ANGLE_LIMIT_MIN);
         // apply deadband
         angle = apply_deadband(params->angInfo.AngleVal, PITCH_ADJ_ANGLE_DEADBAND);
+//        pid->kp = 500 + ABS(exp_angle - angle) * 200;
         // PID control
         pid_loop(pid, exp_angle, angle);
 //        pid->Output += ((0 - angle) * 300 - AngleInfo.AngleRateVal) * 1.0f;
-        pid->Output = pid->Output - params->angInfo.AngleRateVal * 40.6667f;
+//        pid->Output = pid->Output + params->angInfo.AngleRateVal * 10.6667f;
       } else {
         // reset PID controller
         pid->I_sum = 0;
@@ -112,9 +126,9 @@ void ctrl_task(void const *arg)
       pwm16_period((uint32_t)ABS(pid->Output));
       // set direction
       if(pid->Output < 0) {
-        output_port_set(IO_OUTPUT1);
-      } else {
         output_port_clear(IO_OUTPUT1);
+      } else {
+        output_port_set(IO_OUTPUT1);
       }
 
       // yaw axis control
@@ -141,6 +155,37 @@ void ctrl_task(void const *arg)
     }
   }
 }
+#else
+
+#define CTRL_LOOP_PERIOD_MS        (20)           /* 20ms */
+
+void ctrl_task(void const *arg)
+{
+  Params_t *params;
+  params = kmm_alloc(sizeof(Params_t));
+  if(params == NULL) {
+    kmm_free(params);
+#if CONFIG_LOG_ENABLE
+    ky_err(TAG, "no enough memory");
+#endif /* CONFIG_LOG_ENABLE */
+    vTaskDelete(NULL);
+  }
+  ctrlThread = xTaskGetCurrentTaskHandle();
+
+  /* initialize task sync timer */
+  tim7_init(CTRL_LOOP_PERIOD_MS, ctrl_task_notify);
+
+  pwm16_init();
+  pwm17_init();
+  delay(1000); // wait motor driver ready.
+
+  for(;;) {
+    if(xTaskNotifyWait(0xFFFFFFFF, 0xFFFFFFFF, NULL, 100) == pdTRUE) {
+      // ...
+    }
+  }
+}
+#endif /* CTRL_STABILIZE_MODE_ENABLE */
 
 static void ctrl_task_notify(void)
 {
