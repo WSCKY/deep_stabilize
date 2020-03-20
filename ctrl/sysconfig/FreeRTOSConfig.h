c540a963fe975ff86a0544f62a742fa71a819331
kyChu <hello.kychu@gmail.com>
2020-03-20 09:20:55 +0800

    config RTOS kernel

--- ctrl/sysconfig/FreeRTOSConfig.h
+++ ctrl/sysconfig/FreeRTOSConfig.h
@@ -92,8 +92,8 @@
 #include "config.h"
 
 #define configUSE_PREEMPTION			1
-#define configUSE_IDLE_HOOK			0
-#define configUSE_TICK_HOOK			0
+#define configUSE_IDLE_HOOK			1
+#define configUSE_TICK_HOOK			1
 #define configCPU_CLOCK_HZ			( SystemCoreClock )
 #define configTICK_RATE_HZ			( ( TickType_t ) 1000 )
 #define configMAX_PRIORITIES			(  7 )
@@ -110,7 +110,7 @@
 #define configIDLE_SHOULD_YIELD			1
 #define configUSE_MUTEXES			1
 #define configQUEUE_REGISTRY_SIZE		8
-#define configCHECK_FOR_STACK_OVERFLOW	        1
+#define configCHECK_FOR_STACK_OVERFLOW	        0
 #define configUSE_RECURSIVE_MUTEXES		1
 #define configUSE_MALLOC_FAILED_HOOK	        0
 #define configUSE_APPLICATION_TASK_TAG	        0
@@ -130,8 +130,8 @@
 
 /* Set the following definitions to 1 to include the API function, or zero
 to exclude the API function. */
-#define INCLUDE_vTaskPrioritySet		1
-#define INCLUDE_uxTaskPriorityGet		1
+#define INCLUDE_vTaskPrioritySet		0
+#define INCLUDE_uxTaskPriorityGet		0
 #define INCLUDE_vTaskDelete			1
 #define INCLUDE_vTaskCleanUpResources	        0
 #define INCLUDE_vTaskSuspend			0
@@ -160,6 +160,11 @@ standard names. */
 /* IMPORTANT: This define MUST be commented when used with STM32Cube firmware, 
               to prevent overwriting SysTick_Handler defined within STM32Cube HAL */
 #define xPortSysTickHandler SysTick_Handler
+
+#define traceTASK_SWITCHED_IN()  extern void StartIdleMonitor(void); \
+                                    StartIdleMonitor()
+#define traceTASK_SWITCHED_OUT() extern void EndIdleMonitor(void); \
+                                    EndIdleMonitor()
 
 #endif /* FREERTOS_CONFIG_H */
 
